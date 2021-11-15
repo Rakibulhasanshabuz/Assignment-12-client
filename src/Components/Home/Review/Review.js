8b9e4601f8db404ed30afcde4587566be8c6be97
Rakib Hasan <rakibhasan010141@gmail.com>
2021-11-15 21:00:16 +0600

    all-finished

--- src/Components/Home/Review/Review.js
+++ src/Components/Home/Review/Review.js
@@ -12,7 +12,7 @@ const Review = () => {
             <h1 className="text-warning my-5">Our Reviews</h1>
             <div className="row g-5 service-container">
             {
-                reviews.slice(0,6).map(review => <div className="g-4 col-lg-4 col-md-6 col-sm-12 service pb-3">
+                reviews.map(review => <div className="g-4 col-lg-4 col-md-6 col-sm-12 service pb-3">
                 <h1>Name: {review.name}</h1>
                 <h3>Ratting: {review.ratting}</h3>
                 <p>{review.description}</p>
